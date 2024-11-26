c8cea424b9c4bb007fbb695390e7909d0d8ef4ba
RodATS <77297145+RodATS@users.noreply.github.com>
2024-11-25 22:13:29 -0500

    Create hpa.tf

---
+++ Terraform/project/kubernetes_resources/hpa.tf
@@ -0,0 +1,28 @@
+resource "kubernetes_horizontal_pod_autoscaler" "backend_hpa" {
+  metadata {
+    name      = "backend-hpa"
+    namespace = kubernetes_namespace.app_namespace.metadata[0].name
+  }
+
+  spec {
+    scale_target_ref {
+      api_version = "apps/v1"
+      kind        = "Deployment"
+      name        = kubernetes_deployment.backend.metadata[0].name
+    }
+
+    min_replicas = 2
+    max_replicas = 6
+
+    metric {
+      type = "Resource"
+      resource {
+        name = "cpu"
+        target {
+          type               = "Utilization"
+          average_utilization = 50
+        }
+      }
+    }
+  }
+}
